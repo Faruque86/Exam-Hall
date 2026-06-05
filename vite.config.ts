3f56b7450c3e7d256e8d92baca7ed07e8febc6f8
Faruque86 <67383755+Faruque86@users.noreply.github.com>
2026-06-05 23:01:26 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-
+export default defineConfig(() => {
+  return {
+    base: './', // Now assets are resolved relatively, fixing the blank screen!
+    ...
+  }
+});
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
